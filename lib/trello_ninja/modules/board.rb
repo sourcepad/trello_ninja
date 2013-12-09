f8a104f4a4f369c6e507dcfb492c13d1cd591078
Migz Suelto <michaels@sourcepad.com>
2013-12-09 23:48:33 +0800

    trello: skip card that was updated because of the position

--- lib/trello_ninja/modules/board.rb
+++ lib/trello_ninja/modules/board.rb
@@ -21,7 +21,7 @@ module TrelloNinja
         actions.each do |action|
           new_actions << action_message(members, cards, action)
         end
-        return new_actions				
+        return new_actions.compact
       end
 
       private
@@ -38,66 +38,78 @@ module TrelloNinja
           author_username = action['memberCreator']['username'] if action && action['memberCreator']
           member_fullname = action["member"]["fullName"] if action["member"]
           
-          
-          # Create card
-          if action["type"] == "createCard"
-            message = "#{author_fullname} added <a href='#{card_url}' target='_blank'>#{card_name}</a> to #{data["list"]["name"]}"
+          # Do not include if just the position
+          skip = true if action["type"] == "updateCard" && data["old"] && data["old"]["pos"]
 
-          # Move card from old list to new list
-          elsif action['type'] == "updateCard" && data["listAfter"] && data["listBefore"]
-            url = card_url.present? ? "<a href='#{card_url}' target='_blank'>#{card_name}</a>" : "#{card_name}"
-            message = "#{author_fullname} moved #{url} from #{data["listBefore"]["name"]} to #{data["listAfter"]["name"]}."
+          unless skip == true
+            # Create card
+            if action["type"] == "createCard"
+              message = "#{author_fullname} added <a href='#{card_url}' target='_blank'>#{card_name}</a> to #{data["list"]["name"]}"
 
-          # Update the name of a card
-          elsif action["type"] == "updateCard" && data["old"] && data["old"]["name"]
-            message = "#{author_fullname} renamed [#{data["old"]["name"]}] to  <a href='#{card_url}' target='_blank'>#{card_name}</a>"
+            # Move card from old list to new list
+            elsif action['type'] == "updateCard" && data["listAfter"] && data["listBefore"]
+              url = card_url.present? ? "<a href='#{card_url}' target='_blank'>#{card_name}</a>" : "#{card_name}"
+              message = "#{author_fullname} moved #{url} from #{data["listBefore"]["name"]} to #{data["listAfter"]["name"]}."
 
-          # archived card
-          elsif action["type"] == "updateCard" && data["old"]
-            if data["old"]["closed"] == true
-              message = "#{author_fullname} un-archived  <a href='#{card_url}' target='_blank'>#{card_name}</a>"
-            elsif data["old"]["closed"] == false
-              message = "#{author_fullname} archived #{card_name}"
+            # Update the name of a card
+            elsif action["type"] == "updateCard" && data["old"] && data["old"]["name"]
+              message = "#{author_fullname} renamed [#{data["old"]["name"]}] to  <a href='#{card_url}' target='_blank'>#{card_name}</a>"
+
+            # archived card
+            elsif action["type"] == "updateCard" && data["old"]
+              if data["old"]["closed"] == true
+                message = "#{author_fullname} un-archived  <a href='#{card_url}' target='_blank'>#{card_name}</a>"
+              elsif data["old"]["closed"] == false
+                message = "#{author_fullname} archived #{card_name}"
+              end
+
+            # update description
+            elsif action["type"] == "updateCard" && data["old"] && data["old"]["desc"]
+              message = "#{author_fullname} updated the description of <a href='#{card_url}' target='_blank'>#{card_name}</a>"            
+
+            # update position
+            # elsif action["type"] == "updateCard" && data["old"] && data["old"]["pos"]
+            #   message = "none"            
+
+            # Comment on card
+            elsif action['type'] == "commentCard"
+              message = "#{author_fullname} commented on <a href='#{card_url}' target='_blank'>#{card_name}</a>: \"#{data["text"]}\""
+
+            # Add a member to a card
+            elsif action["type"] == "addMemberToCard"
+              if action["memberCreator"]["id"] == action["member"]["id"]
+                message = "#{author_fullname} joined <a href='#{card_url}' target='_blank'>#{card_name}</a>"
+              else
+                message = "#{author_fullname} added #{member_fullname} to <a href='#{card_url}' target='_blank'>#{card_name}</a>"
+              end          
+
+            # Remove a member to a card
+            elsif action["type"] == "removeMemberFromCard"
+              if action["memberCreator"]["id"] == action["member"]["id"]
+                message = "#{author_fullname} left <a href='#{card_url}' target='_blank'>#{card_name}</a>"
+              else
+                message = "#{author_fullname} removed #{member_fullname} from <a href='#{card_url}' target='_blank'>#{card_name}</a>"
+              end
+
+            elsif action["type"] == "addMemberToBoard"
+              begin
+                added_member_object = members.detect { |member| member['id'] == data['idMemberAdded']}
+                message = "#{author_fullname} added #{added_member_object.present? ? added_member_object['fullName'] : 'a member'} to #{data['board']['name']}"
+              rescue
+                binding.pry
+              end
+
+            # Complete an item in the checklist of a card
+            elsif action["type"] == "updateCheckItemStateOnCard" && data["checkItem"]["state"] == "complete"
+              message = "#{author_fullname} completed #{data["checkItem"]["name"]} on <a href='#{card_url}' target='_blank'>#{card_name}</a>"            
             end
-
-          elsif action["type"] == "updateCard" && data["old"] && data["old"]["desc"]
-            message = "#{author_fullname} updated the description of <a href='#{card_url}' target='_blank'>#{card_name}</a>"            
-
-          # Comment on card
-          elsif action['type'] == "commentCard"
-            message = "#{author_fullname} commented on <a href='#{card_url}' target='_blank'>#{card_name}</a>: \"#{data["text"]}\""
-
-          # Add a member to a card
-          elsif action["type"] == "addMemberToCard"
-            if action["memberCreator"]["id"] == action["member"]["id"]
-              message = "#{author_fullname} joined <a href='#{card_url}' target='_blank'>#{card_name}</a>"
-            else
-              message = "#{author_fullname} added #{member_fullname} to <a href='#{card_url}' target='_blank'>#{card_name}</a>"
-            end          
-
-          # Remove a member to a card
-          elsif action["type"] == "removeMemberFromCard"
-            if action["memberCreator"]["id"] == action["member"]["id"]
-              message = "#{author_fullname} left <a href='#{card_url}' target='_blank'>#{card_name}</a>"
-            else
-              message = "#{author_fullname} removed #{member_fullname} from <a href='#{card_url}' target='_blank'>#{card_name}</a>"
-            end
-
-          elsif action["type"] == "addMemberToBoard"
-            begin
-              added_member_object = members.detect { |member| member['id'] == data['idMemberAdded']}
-              message = "#{author_fullname} added #{added_member_object.present? ? added_member_object['fullName'] : 'a member'} to #{data['board']['name']}"
-            rescue
-              binding.pry
-            end
-
-          # Complete an item in the checklist of a card
-          elsif action["type"] == "updateCheckItemStateOnCard" && data["checkItem"]["state"] == "complete"
-            message = "#{author_fullname} completed #{data["checkItem"]["name"]} on <a href='#{card_url}' target='_blank'>#{card_name}</a>"            
+            
+            action['link'] = card_url
+            action['message'] = message
+          else
+            action = nil
           end
-          
-          action['link'] = card_url
-          action['message'] = message
+
           return action
         end
     end
